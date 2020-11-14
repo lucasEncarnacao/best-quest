2fba2c5e2f654703656cd8c027dd7650d95e99ff
Lucas Encarnacao <lucas.encarnacao5@gmail.com>
2020-11-14 12:14:12 -0500

    WIP Adding colors

--- app/javascript/react/components/theme.js
+++ app/javascript/react/components/theme.js
@@ -1,14 +1,13 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { deepPurple, amber } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: deepPurple[500],
+      main: '#52BEEB',
     },
     secondary: {
-      main: amber[500],
-      contrastText: deepPurple[900],
+      main: '#F04664',
+      contrastText: '#FFFFFF',
     },
   },
 });
